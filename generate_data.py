79b02641ab852652ca47ac5d47c265d57fdd7f20
byungok.han <byungok.han@etri.re.kr>
2024-01-20 11:48:20 +0900

    added llava

--- generate_data.py
+++ generate_data.py
@@ -28,31 +28,31 @@ def describe_all_bboxes_with_chatgpt(image_path, bboxes, goal_label_cxcy):
     dest_descriptions = f"{goal_label} at ({goal_cxcy[0]}, {goal_cxcy[1]})"
 
     # GPT-4에 대한 프롬프트 구성
-    prompt = (  "[Context: An image depicts the view from a pedestrian's position, " 
+    prompt = (  "[Context: The input image depicts the view from a pedestrian's position, " 
                 "taken at a point 80cm above the ground for pedestrian navigation purposes. " 
-                "In this image, the user's starting point is situated below the center of the image at (0.5, 1). "
+                "In this image, the user's starting point is situated below the center of the image at (0.5, 1.0). "
                 "Consider the starting point as the ground where the user is standing.]\n" 
                 f"[Obstacle Name at (bounding box): [{bbox_list_str}].]\n"              
                 f"[Destination Name at (point): [{dest_descriptions}].]\n"
                 "Describe the obstacles to the destination in a natural and simple way "
                 "for a visually impaired person as a navigation assistant in 3 sentences. "
-                "Don't talk about detailed image coordinates and consider perspective view of the 2D image property. "
+                "Don't talk about detailed image coordinates. Consider perspective view of the 2D image property. "
                 "First, answer it in English, then translate it Korean.")
    
     print("[PROMPT]: ", prompt)
     # OpenAI API 키 설정 (환경 변수에서 가져옴)
     openai.api_key = OPENAI_API_KEY
     completion = openai.chat.completions.create(
-        model = "gpt-4",
-        #model="gpt-4-1106-preview",
+        #model = "gpt-4",
+        model="gpt-4-1106-preview",
         #messages=[
         #    {
         #        "role": "user",
         #        "content": prompt,
         #    },
         messages=[
-#            {"role": "system", "content": "This is an image-based task."},
-#            {"role": "user", "content": encoded_image}, #, "mimetype": "image/jpeg"
+            {"role": "system", "content": "This is an image-based task."},
+            {"role": "user", "content": encoded_image}, #, "mimetype": "image/jpeg"
             {"role": "user", "content": prompt},
         ],
         #max_tokens=1000,
@@ -208,10 +208,10 @@ def main():
             draw.ellipse([goal_point_lu_draw, goal_point_rd_draw], fill='red')
 
             ## 이미지 및 Bounding Box 표시
-            #plt.imshow(img)
-            #plt.axis('off')
-            #plt.title(img_file + f', goal_label:{goal_label}')
-            #plt.show()
+            plt.imshow(img)
+            plt.axis('off')
+            plt.title(img_file + f', goal_label:{goal_label}')
+            plt.show()
 
             final_answer = description
 
