fef9f9cf04eeddee74eb83ecc709f7ed2295d209
byungok.han <byungok.han@etri.re.kr>
2024-01-26 15:12:54 +0900

    print added

--- vlm_description.py
+++ vlm_description.py
@@ -81,6 +81,9 @@ def describe_all_bboxes_with_llava(llava_model_base_path, llava_model_path, imag
                 "Don't talk about detailed image coordinates. Consider perspective view of the 2D image property. "
                 "First, answer it in English, then translate it Korean.")
 
+    print("[PROMPT] ====================================================")
+    print(prompt)
+
     args = type('Args', (), {
         "model_path": llava_model_path,
         "model_base": llava_model_base_path,
@@ -94,5 +97,10 @@ def describe_all_bboxes_with_llava(llava_model_base_path, llava_model_path, imag
         "num_beams": 1,
         "max_new_tokens": 512
     })()
+    
+    answer = eval_model(args)
+
+    print("[ANSWER] ====================================================")
+    print(answer)
 
     return eval_model(args)
